3de355c9525c369851d8ce45ea0ecb54a363c31a
eversoncoutinho <eversoncoutinho@gmail.com>
2022-03-18 15:52:47 +0000

    Init

---
+++ Malabarista.Domain/Entities/Entity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malabarista.Domain.Entities
+{
+    public abstract class Entity
+        {
+            public virtual long Id { get; protected set; } //identificador
+                                                           //metodos para verificar a igualdade de objetos
+            public override bool Equals(object obj)
+            {
+                var other = obj as Entity;
+                if (ReferenceEquals(other, null))
+                    return false;
+                if (ReferenceEquals(this, other))
+                    return true;
+                if (Id == 0 || other.Id == 0)
+                    return false;
+                return Id == other.Id;
+            }
+            public static bool operator ==(Entity a, Entity b)
+            {
+                if (ReferenceEquals(a, null) && ReferenceEquals(a, null))
+
+                    return true;
+                if (ReferenceEquals(a, null) || ReferenceEquals(a, null))
+                    return false;
+                return a.Equals(b);
+
+            }
+            public static bool operator !=(Entity a, Entity b)
+            {
+                return !(a == b);
+            }
+            public override int GetHashCode()
+            {
+                return (GetType().ToString() + Id).GetHashCode();
+            }
+        }
+    }
+
---
+++ Malabarista.Domain/ValueObjects/ValueObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malabarista.Domain.ValueObjects
+
+{
+    //Classe base do OV
+    public abstract class ValueObject<T>
+        where T : ValueObject<T>
+    {
+        //igualdade estrutural
+        public override bool Equals(object valueObject)
+        {
+            var objDeValor = valueObject as T;
+
+            if (ReferenceEquals(objDeValor, null))
+                return false;
+
+            return EqualsCore(objDeValor);
+        }
+
+        protected abstract bool EqualsCore(T other);
+
+        public override int GetHashCode()
+        {
+            return (int)GetHashCodeCore();
+        }
+
+        protected abstract decimal GetHashCodeCore();
+
+        public static bool operator ==(ValueObject<T> objectA, ValueObject<T> objectB)
+        {
+            if (ReferenceEquals(objectA, null) && ReferenceEquals(objectB, null))
+                return true;
+
+            return objectA.Equals(objectB);
+        }
+
+        public static bool operator !=(ValueObject<T> objectA, ValueObject<T> objectB)
+        {
+            return !(objectA == objectB);
+        }
+
+    }
+}
