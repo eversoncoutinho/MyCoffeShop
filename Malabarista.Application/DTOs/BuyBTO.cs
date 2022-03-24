131975a44127c7ba80205290e422a6efc65ab98f
eversoncoutinho <eversoncoutinho@gmail.com>
2022-03-24 18:46:27 +0000

    Inserção do projeto de testes unitários

---
+++ Malabarista.Application/DTOs/BuyBTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malabarista.Application.DTOs
+{
+    public class BuyBTO
+    {
+        public GrainDTO Grain { get; set; }
+        public int Quantity { get; private set; }
+        public decimal Shippiment { get; private set; }
+        public int LoteNumber { get; private set; }
+        public DateTime RoasteDate { get; private set; }
+        public DateTime BuyDate { get; private set; }
+    }
+}
